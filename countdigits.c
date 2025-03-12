8a2eb84622e5865d93c9fca5cfe789d7524df2e5
Triveni mungara <trivenimungara@gmail.com>
2025-03-12 16:27:07 +0530

    add cpratice set-2 countdigits
    Signed-off-by: Triveni mungara <trivenimungara@gmail.com>

--- countdigits.c
+++ countdigits.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 void main()
 {
-    int count=0,num;
+    int count=0,num=0;
     printf("enter the number:");
     scanf("%d",&num);
     if(num==0)
 	    count=1;
     while(num!=0)
+    {
 	    count++;
+            num=num/10;
+    }
     printf("total number of digits:%d",count);
 }
 
