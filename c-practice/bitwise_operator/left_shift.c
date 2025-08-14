edf6468b5cbf9223d2a79e0538b07f1f91ebbf3c
Triveni Mungara <trivenimungara@gmail.com>
2025-08-14 17:24:03 +0530

    test
    Signed-off-by: Triveni Mungara <trivenimungara@gmail.com>

---
+++ c-practice/bitwise_operator/left_shift.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+
+int main() {
+  
+    unsigned char a = 21;
+
+    printf("a << 1 = %d\n", (a << 1));
+
+    return 0;
+}
---
+++ c-practice/bitwise_operator/right_shift.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+
+
+int main()
+{
+    
+    unsigned char a = 21;
+    printf("a >> 1 = %d\n", (a >> 1));
+
+    return 0;
+}
