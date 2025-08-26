149f01c89a1b584d0c8ca0af67fded04521d23c4
Triveni Mungara <trivenimungara@gmail.com>
2025-08-26 11:39:32 +0530

    test
    Signed-off-by: Triveni Mungara <trivenimungara@gmail.com>

--- c-practice/arrays/sum_matrix.c
+++ c-practice/arrays/sum_matrix.c
@@ -1,36 +1,36 @@
 #include <stdio.h>
 
 int main() {
-    int i, j, p;
-    int a[3][3][3] = {0};
-    int b[3][3][3] = {0};
-    int sum[3][3][3];
+    int i, j;
+    int a[3][3] = {0};
+    int b[3][3] = {0};
+    int sum[3][3];
 
-    printf("Enter elements of array A (27 numbers):\n");
+    printf("Enter elements of array A:\n");
     for(i=0; i<3; i++)
         for(j=0; j<3; j++)
-            for(p=0; p<3; p++)
-                scanf("%d", &a[i][j][p]);
+            
+                scanf("%d", &a[i][j]);
 
-    printf("Enter elements of array B (27 numbers):\n");
+    printf("Enter elements of array B :\n");
     for(i=0; i<3; i++)
         for(j=0; j<3; j++)
-            for(p=0; p<3; p++)
-                scanf("%d", &b[i][j][p]);
+           
+                scanf("%d", &b[i][j]);
 
     // Calculate sum
     for(i=0; i<3; i++)
         for(j=0; j<3; j++)
-            for(p=0; p<3; p++)
-                sum[i][j][p] = a[i][j][p] + b[i][j][p];
+         
+                sum[i][j] = a[i][j] + b[i][j];
 
     // Print result
-    printf("\nSum of arrays:\n");
+    printf("Sum of arrays:\n");
     for(i=0; i<3; i++) {
         for(j=0; j<3; j++) {
-            for(p=0; p<3; p++)
-                printf("%3d ", sum[i][j][p]);
-            printf("\t");
+          
+                printf("%d ", sum[i][j]);
+          
         }
         printf("\n");
     }
