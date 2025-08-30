8e6d0578112ad7e0efad5bb1d7354ec8cb84683f
Triveni Mungara <trivenimungara@gmail.com>
2025-08-30 17:17:53 +0530

    test
    Signed-off-by: Triveni Mungara <trivenimungara@gmail.com>

---
+++ c-practice/patterns/sq_num.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	int n,i,j;
+	printf("enter n:");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=n;j++)
+		{
+			printf("%d",i);
+		}
+	         printf("\n");
+	}
+		
+}
