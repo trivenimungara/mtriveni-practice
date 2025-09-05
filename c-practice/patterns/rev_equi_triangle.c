2b99dcba609e65802104367e140fddbee6f7d156
Triveni Mungara <trivenimungara@gmail.com>
2025-09-05 18:49:14 +0530

    test
    Signed-off-by: Triveni Mungara <trivenimungara@gmail.com>

---
+++ c-practice/patterns/rev_equi_triangle.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &n);
+
+    for(i = n; i >= 1; i--) {
+        
+        for(j = 0; j < n - i; j++) {
+            printf(" ");
+        }
+        
+        for(j = 1; j <= i; j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
