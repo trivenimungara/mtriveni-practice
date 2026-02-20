cb044f834ee0b9167d1119b7fe01690fb04c7504
Triveni Mungara <trivenimungara@gmail.com>
2026-02-20 10:10:18 +0530

    test
    Signed-off-by: Triveni Mungara <trivenimungara@gmail.com>

---
+++ c-practice/functions/swap_2nums.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void swap(int *a, int *b) {
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+int main() {
+    int x = 5, y = 10;
+    swap(&x, &y);
+    printf("%d %d\n", x, y);
+    return 0;
+}
+
